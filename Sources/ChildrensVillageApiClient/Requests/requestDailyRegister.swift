cdbdf18cc0a5313b191521c57b003e0eb3d28770
Chris Kobrzak <chris.kobrzak@gmail.com>
2021-09-04 00:27:50 +0100

    Do not manually encode URL query string
    Copied from Leo Dabus's StackOverflow answer:
    https://stackoverflow.com/a/43668198/764906

--- Sources/ChildrensVillageApiClient/Requests/requestDailyRegister.swift
+++ Sources/ChildrensVillageApiClient/Requests/requestDailyRegister.swift
@@ -8,8 +8,6 @@
 import Foundation
 import JwtApiClient
 
-let baseApiUrl = "https://childrens-village.co.uk/api"
-
 @available(iOS 15.0.0, *)
 public func requestDailyRegister(
   branchId: Int,
@@ -18,11 +16,23 @@ public func requestDailyRegister(
 ) async throws -> [Pupil] {
   let urlFilter = buildDailyRegisterRequestFilter(dayOfWeek)
   let filterJson = JSONEncoder.encode(from: urlFilter)
-  // TODO URL decoding helper needed
-  let escapedJson = filterJson.addingPercentEncoding(withAllowedCharacters: .alphanumerics)
-  let endpoint = "\(baseApiUrl)/branches/\(branchId)?filter=\(escapedJson ?? "empty")"
+
+  guard let endpoint = buildDailyRegisterUrlComponent(branchId: branchId, filter: filterJson).url else { return [] }
 
   let result: PupilsByDayByBranch = try await requestWithAuthorisation(endpoint, token: token)
 
   return result.daysOfWeek.first?.pupils ?? []
+}
+
+func buildDailyRegisterUrlComponent(branchId: Int, filter: String) -> URLComponents {
+  let path = "/api/branches/\(branchId)"
+  let queryItem = URLQueryItem(name: "filter", value: filter)
+
+  var urlComponent = URLComponents()
+  urlComponent.scheme = "https"
+  urlComponent.host = "childrens-village.co.uk"
+  urlComponent.path = path
+  urlComponent.queryItems = [queryItem]
+
+  return urlComponent
 }
