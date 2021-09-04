77ee21bf92d6239dbb0a7bb93c476c463766ae8e
Chris Kobrzak <chris.kobrzak@gmail.com>
2021-09-04 23:12:38 +0100

    Add integration tests for login requests

--- Tests/ChildrensVillageApiClientTests/ApiClientIntegrationTests.swift
+++ Tests/ChildrensVillageApiClientTests/ApiClientIntegrationTests.swift
@@ -2,23 +2,49 @@ import XCTest
 @testable import ChildrensVillageApiClient
 @testable import JwtApiClient
 
-// You will need to re-generate this token for the test to pass
-let jwtToken = "<valid.token.here>"
-
+@available(iOS 15.0.0, *)
 class ApiClientIntegrationTests: XCTestCase {
-  @available(iOS 15.0.0, *)
+  var jwtToken = ""
+  let baseUrl = "https://childrens-village.co.uk/api"
+
+  override func setUp() async throws {
+    let url = URL(string: "\(baseUrl)/users/login")
+    let credentials = [
+      "email": "<login>",
+      "password": "<password>"
+    ]
+
+    do {
+      let result: Token = try await requestTokenWithJsonCredentials(url!, credentials)
+      jwtToken = result.token
+    } catch {
+      print("Authentication failed. Please enter correct credentials and try again.")
+    }
+  }
+
+  func testRequestTokenWithJsonCredentials() async throws {
+    let url = URL(string: "\(baseUrl)/users/login")
+    let credentials = [
+      "email": "joe@bloggs.com",
+      "password": "wrongPassword"
+    ]
+
+    let result: TokenError = try await requestTokenWithJsonCredentials(url!, credentials)
+
+    print("Error details: \(result.error.name)")
+  }
+
   func testRequestWithAuthorisation() async throws {
     let branchId = 1
     let dayOfTheWeek = "Monday"
 
-    let endpoint = "https://childrens-village.co.uk/api/branches/\(branchId)?filter=%7B%22include%22%3A%5B%7B%22relation%22%3A%22daysOfWeek%22%2C%22scope%22%3A%7B%22where%22%3A%7B%22day%22%3A%22\(dayOfTheWeek)%22%7D%2C%22include%22%3A%5B%7B%22relation%22%3A%22pupils%22%2C%22scope%22%3A%7B%22order%22%3A%22firstName%22%7D%7D%5D%7D%7D%5D%7D"
+    let endpoint = "\(baseUrl)/branches/\(branchId)?filter=%7B%22include%22%3A%5B%7B%22relation%22%3A%22daysOfWeek%22%2C%22scope%22%3A%7B%22where%22%3A%7B%22day%22%3A%22\(dayOfTheWeek)%22%7D%2C%22include%22%3A%5B%7B%22relation%22%3A%22pupils%22%2C%22scope%22%3A%7B%22order%22%3A%22firstName%22%7D%7D%5D%7D%7D%5D%7D"
 
     let result: PupilsByDayByBranch = try await requestWithAuthorisation(endpoint, token: jwtToken)
 
     assert(result.id == branchId)
   }
 
-  @available(iOS 15.0.0, *)
   func testRequestDailyRegister() async throws {
     let branchId = 1
     let dayOfTheWeek = DayOfWeek.Monday
