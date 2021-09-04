124a75ae04f89e29a60fde6a668f14f8a52111b5
Chris Kobrzak <chris.kobrzak@gmail.com>
2021-09-04 22:07:03 +0100

    Add unit test for objtaining tokens
    This requires a valid JWT token and has been added for illustration
    purposes only.

---
+++ Tests/ChildrensVillageApiClientTests/ApiClientTest.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import ChildrensVillageApiClient
+@testable import JwtApiClient
+
+// You will need to re-generate this token for the test to pass
+let jwtToken = "<valid.token.here>"
+
+class ApiClientTest: XCTestCase {
+  @available(iOS 15.0.0, *)
+  func testRequestWithAuthorisation() async throws {
+    let branchId = 1
+    let dayOfTheWeek = "Monday"
+
+    let endpoint = "https://childrens-village.co.uk/api/branches/\(branchId)?filter=%7B%22include%22%3A%5B%7B%22relation%22%3A%22daysOfWeek%22%2C%22scope%22%3A%7B%22where%22%3A%7B%22day%22%3A%22\(dayOfTheWeek)%22%7D%2C%22include%22%3A%5B%7B%22relation%22%3A%22pupils%22%2C%22scope%22%3A%7B%22order%22%3A%22firstName%22%7D%7D%5D%7D%7D%5D%7D"
+
+    let result: PupilsByDayByBranch = try await requestWithAuthorisation(endpoint, token: jwtToken)
+
+    assert(result.id == branchId)
+  }
+}
