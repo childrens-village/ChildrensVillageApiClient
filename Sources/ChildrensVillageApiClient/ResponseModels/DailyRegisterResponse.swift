281b4d0d86de1fb449978ec4b46db2b3eb65bede
Chris Kobrzak <chris.kobrzak@gmail.com>
2021-09-06 22:10:25 +0100

    Decouple response models from error handling extensions
    The intention is to only define error handling extensions in model files
    representing JSON objects returned by the API, not their child nodes.
    This distinction may become a little blurry over time as some models
    (e.g. Pupil) might eventually become top-level response models and
    require the extensions.
    
    Since both kinds are models are so close related to one another, they
    are stored under the same directory for now.

--- Sources/ChildrensVillageApiClient/ResponseModels/DailyRegisterResponse.swift
+++ Sources/ChildrensVillageApiClient/ResponseModels/DailyRegisterResponse.swift
@@ -1,3 +1,26 @@
 import Foundation
 
 public typealias DailyRegisterResponse = PupilsByDayByBranch
+
+extension DailyRegisterResponse {
+  enum CodingKeys: CodingKey {
+    case id
+    case name
+    case geolocation
+    case address
+    case daysOfWeek
+  }
+
+  public init(from decoder: Decoder) throws {
+    if let container = try? decoder.container(keyedBy: CodingKeys.self),
+       let id = try? container.decode(Int.self, forKey: .id),
+       let name = try? container.decode(String.self, forKey: .name),
+       let geolocation = try? container.decode(Geolocation.self, forKey: .geolocation),
+       let address = try? container.decode(String.self, forKey: .address),
+       let daysOfWeek = try? container.decode([PupilsByDay].self, forKey: .daysOfWeek) {
+      self.init(id: id, name: name, geolocation: geolocation, address: address, daysOfWeek: daysOfWeek)
+    } else {
+      throw try ErrorResponse(from: decoder).error
+    }
+  }
+}
--- Sources/ChildrensVillageApiClient/ResponseModels/PupilsByDayByBranch.swift
+++ Sources/ChildrensVillageApiClient/ResponseModels/PupilsByDayByBranch.swift
@@ -7,26 +7,3 @@ public struct PupilsByDayByBranch: Decodable {
   let address: String
   let daysOfWeek: [PupilsByDay]
 }
-
-extension PupilsByDayByBranch {
-  enum CodingKeys: CodingKey {
-    case id
-    case name
-    case geolocation
-    case address
-    case daysOfWeek
-  }
-
-  public init(from decoder: Decoder) throws {
-    if let container = try? decoder.container(keyedBy: CodingKeys.self),
-       let id = try? container.decode(Int.self, forKey: .id),
-       let name = try? container.decode(String.self, forKey: .name),
-       let geolocation = try? container.decode(Geolocation.self, forKey: .geolocation),
-       let address = try? container.decode(String.self, forKey: .address),
-       let daysOfWeek = try? container.decode([PupilsByDay].self, forKey: .daysOfWeek) {
-      self.init(id: id, name: name, geolocation: geolocation, address: address, daysOfWeek: daysOfWeek)
-    } else {
-      throw try ErrorResponse(from: decoder).error
-    }
-  }
-}
