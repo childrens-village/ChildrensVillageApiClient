6d12d413fa2979d632d970a5565bd8904b2053ff
Chris Kobrzak <chris.kobrzak@gmail.com>
2025-11-03 10:12:30 +0000

    Address Swift 6 compiler warning

--- Sources/ChildrensVillageApiClient/DateUtilities/DayOfWeek.swift
+++ Sources/ChildrensVillageApiClient/DateUtilities/DayOfWeek.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public enum DayOfWeek: String, CaseIterable, Codable, Equatable {
+public enum DayOfWeek: String, CaseIterable, Codable, Equatable, Sendable {
   case Monday
   case Tuesday
   case Wednesday
